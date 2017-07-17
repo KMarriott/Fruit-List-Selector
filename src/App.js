e451f7c59938c390efe0c1a929070881a20b7730
Khem Marriott <KhemMarriott@Gmail.com>
2017-07-16 23:10:49 -0400

    small fix

--- src/App.js
+++ src/App.js
@@ -21,17 +21,17 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to the Default React Layout!
+          <br/>(With Material-UI)</h2>
 
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
         <ComboBox/>
 
       </div>
-        </MuiThemeProvider>
+      </MuiThemeProvider>
     );
   }
 }
